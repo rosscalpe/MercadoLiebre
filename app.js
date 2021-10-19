153d55acec20cd9ca2ecf8f4dc5473b327263465
rosscalpe <ross.gusteban@gmail.com>
2021-10-19 14:04:10 -0300

    subiendo MercadoLiebre a github

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const app = express ();
+
+const path = require ('path');
+
+app.use (express.static(path.resolve (__dirname,'./public')));
+
+
+app.get ('/', (req,res) => {
+    res.sendFile(path.resolve (__dirname,'./views/home.html'))
+});
+
+app.get ('/register', (req,res) => {
+    res.sendFile(path.resolve (__dirname,'./views/register.html'))
+});
+
+app.get ('/login', (req,res) => {
+    res.sendFile(path.resolve (__dirname,'./views/login.html'))
+});
+
+    app.listen (3000, () => {
+    console.log ('servidor andando')
+});
